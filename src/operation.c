620ff26c0bd21198acb73060ee4308056e0f79c5
lainredsonic <lp007819@hotmail.com>
2014-05-23 14:45:53 +0800

    [update] clean up operation.c

--- src/operation.c
+++ src/operation.c
@@ -2,13 +2,27 @@
 #include <pthread.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <errno.h>
+
 #include "list.h"
 #include "dpu.h"
 #include "operation.h"
 
 struct list_head dpu_list;
-//pthread_mutex_t dpu_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t dpu_mutex;
+
+struct dpu * find_dpu_by_addr(char *serv_addr){
+	struct dpu *tmp_dpu, *find_dpu=NULL;
+	struct in_addr in_addr;
+	inet_pton(AF_INET, serv_addr, &in_addr);
+	list_for_each_entry(tmp_dpu, &dpu_list, lh){
+		if(in_addr.s_addr == tmp_dpu->serv_addr){
+			find_dpu=tmp_dpu;
+			break;
+		}
+	}
+	return find_dpu;
+}
 
 void dpu_init(){
 	INIT_LIST_HEAD(&dpu_list);
@@ -19,15 +33,17 @@ int dpu_add(char *alias, char *serv_addr,
 		char *dname, unsigned short pkg_nr,
 		unsigned short timeout, int health_thres)
 {
-	struct dpu *tmp_dpu;
 	struct in_addr in_addr;
-	inet_pton(AF_INET, serv_addr, &in_addr);
-	list_for_each_entry(tmp_dpu, &dpu_list, lh){
-		if(in_addr.s_addr == tmp_dpu->serv_addr){
-//			return -1;
-		}
+	struct dpu *tmp_dpu = find_dpu_by_addr(serv_addr);
+
+	if(tmp_dpu){
+		return -1;
 	}
 	tmp_dpu = (struct dpu *)malloc(sizeof(struct dpu));
+	if(!tmp_dpu){
+		return -ENOMEM;
+	}
+	inet_pton(AF_INET, serv_addr, &in_addr);
 	INIT_LIST_HEAD(&tmp_dpu->lh);
 	strcpy(tmp_dpu->alias, alias);
 	tmp_dpu->serv_addr = in_addr.s_addr;
@@ -40,15 +56,8 @@ int dpu_add(char *alias, char *serv_addr,
 }
 
 int dpu_del(char *serv_addr){
-	struct dpu *tmp_dpu, *find_dpu=NULL;
-	in_addr_t addr;
-	inet_pton(AF_INET, serv_addr, &addr);
-	list_for_each_entry(tmp_dpu, &dpu_list, lh){
-		if(tmp_dpu->serv_addr == addr){
-			find_dpu=tmp_dpu;
-			break;
-		}
-	}
+	struct dpu *find_dpu = find_dpu_by_addr(serv_addr);
+
 	if(find_dpu){
 		list_del(&find_dpu->lh);
 		free(find_dpu);
@@ -58,15 +67,8 @@ int dpu_del(char *serv_addr){
 }
 
 int dpu_set(char *serv_addr, unsigned short type){
-	struct dpu *tmp_dpu, *find_dpu=NULL;
-	in_addr_t addr;
-	inet_pton(AF_INET, serv_addr, &addr);
-	list_for_each_entry(tmp_dpu, &dpu_list, lh){
-		if(tmp_dpu->serv_addr == addr){
-			find_dpu=tmp_dpu;
-			break;
-		}
-	}
+	struct dpu *find_dpu = find_dpu_by_addr(serv_addr);
+
 	if(!find_dpu)
 		return -1;
 
@@ -88,15 +90,7 @@ int dpu_set(char *serv_addr, unsigned short type){
 }
 
 int dpu_health(char *serv_addr){
-	struct dpu *tmp_dpu, *find_dpu=NULL;
-	in_addr_t addr;
-	inet_pton(AF_INET, serv_addr, &addr);
-	list_for_each_entry(tmp_dpu, &dpu_list, lh){
-		if(tmp_dpu->serv_addr == addr){
-			find_dpu=tmp_dpu;
-			break;
-		}
-	}
+	struct dpu *find_dpu = find_dpu_by_addr(serv_addr);
 	if(!find_dpu)
 		return -1;
 	return find_dpu->health;
